4ce6d523fb4bbaf31a0aa5794d639d8dac28b06d
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-12 16:52:52 +0530

    moved to project-planton

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	stackjobrunnerkubernetesv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/provider/kubernetes/stackjobrunnerkubernetes/v1"
+	stackjobrunnerkubernetesv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/kubernetes/stackjobrunnerkubernetes/v1"
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
-	"github.com/plantoncloud/stack-job-runner-kubernetes-pulumi-module/pkg"
+	"github.com/project-planton/pulumi-module-golang-commons/pkg/stackinput"
+	"github.com/project-planton/stack-job-runner-kubernetes-pulumi-module/pkg"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
--- pkg/main.go
+++ pkg/main.go
@@ -1,9 +1,9 @@
 package pkg
 
 import (
-	stackjobrunnerkubernetesv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/provider/kubernetes/stackjobrunnerkubernetes/v1"
+	stackjobrunnerkubernetesv1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/kubernetes/stackjobrunnerkubernetes/v1"
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/provider/kubernetes/pulumikubernetesprovider"
+	"github.com/project-planton/pulumi-module-golang-commons/pkg/provider/kubernetes/pulumikubernetesprovider"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
